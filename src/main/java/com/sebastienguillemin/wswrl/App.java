75ac6dc2a87f9378bacf1cc226e149b292218689
Sébastien GUILLEMIN <sebastien.guillemin11@gmail.com>
2024-03-06 17:50:59 +0100

    Creating project and gitignore

---
+++ src/main/java/com/sebastienguillemin/wswrl/App.java
@@ -0,0 +1,13 @@
+package com.sebastienguillemin.wswrl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
